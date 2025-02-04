244c91dfe595933f8d18b8d6338b711cc0e4dd45
Khushi chaudhari <khushichaudhari0406@gmail.com>
2025-02-04 17:58:05 +0530

    Add files via upload

---
+++ day 10 static member/staticmemberfunction.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class one{
+    int a,b;
+    public:
+    static int c;
+    void set(int x, int y)
+    {
+        a=x;
+        b=y;
+    }
+    void display()
+    {
+        cout<<a<<" "<<b<<" "<<c<<endl;
+    }
+    static void print()
+    {
+        cout<<c<<endl;
+    }
+};
+
+int one::c;
+
+int main()
+{
+    one ob1;
+    ob1.set(2,3);
+    ob1.display();
+    ob1.print();
+    one::print();  //:: scope resolution operator
+}
